bf40aa294520b77efd03fb32fe3c692734e9ca56
mighty-mite <tushinov.ayur@gmail.com>
2021-03-10 20:19:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array != undefined && array.length > 0) {
+    array.sort((a,b) => a - b);
+    return array[0];
+  }
+  return 0
 }
 
 exports.max = function max (array) {
+  if(array != undefined && array.length > 0) {
+    array.sort((a,b) => a - b);
+    return array[array.length - 1];
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if(array != undefined && array.length > 0) {
+    return array.reduce((sum, current) => sum + current, 0) / array.length;
+  }
   return 0;
 }
